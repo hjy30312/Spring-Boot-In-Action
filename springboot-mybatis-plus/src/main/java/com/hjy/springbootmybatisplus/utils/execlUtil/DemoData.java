62aa01dc95339dd75f5fa5b40e70a9060e9422d3
hjy <543363559@qq.com>
2020-04-19 23:50:52 +0800

    easyexcel读

---
+++ springboot-mybatis-plus/src/main/java/com/hjy/springbootmybatisplus/utils/execlUtil/DemoData.java
@@ -0,0 +1,25 @@
+package com.hjy.springbootmybatisplus.utils.execlUtil;
+
+import lombok.Data;
+
+/**
+ * @author hjy
+ * @date 2020/4/19 22:59
+ */
+@Data
+public class DemoData {
+
+    /**
+     * 学号
+     */
+    private String no;
+    /**
+     * 成绩
+     */
+    private Double score;
+    /**
+     * 创建时间
+     */
+    private Data createTime;
+
+}
---
+++ springboot-mybatis-plus/src/main/java/com/hjy/springbootmybatisplus/utils/execlUtil/TestFileUtil.java
@@ -0,0 +1,39 @@
+package com.hjy.springbootmybatisplus.utils.execlUtil;
+
+import java.io.File;
+import java.io.InputStream;
+
+/**
+ * @author hjy
+ * @date 2020/4/19 22:43
+ */
+public class TestFileUtil {
+
+
+    public static InputStream getResourcesFileStream(String fileName) {
+        return Thread.currentThread().getContextClassLoader().getResourceAsStream("" + fileName);
+    }
+
+
+    public static String getPath() {
+        return TestFileUtil.class.getResource("/").getPath();
+    }
+
+    public static File createNewFile(String pathName) {
+        File file = new File(getPath() + pathName);
+        if (file.exists()) {
+            file.delete();
+        } else {
+            if (!file.getParentFile().exists()) {
+                file.getParentFile().mkdirs();
+            }
+        }
+        return file;
+    }
+
+    public static File readFile(String pathName) {
+        return new File((getPath() + pathName));
+    }
+
+
+}
